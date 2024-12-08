ae7b852fc8667a0cff7311f53409afc11c6efbbc
kevinDsousa <kevindiegodasilvasousa@gmail.com>
2024-12-08 12:17:20 -0300

    test(testes de unidade): :test_tube: Realizado teste no contrutor de category

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,8 +1,13 @@
 import { Category } from "./category";
 
-describe( "Category", () => {
-    test('construtor da entidade', () => {
-        const category = new Category('movie');
-        expect(category.name).toBe('movie');
-    }); 
+describe("Category unit test", () => {
+    test('Construtor da entidade Category', () => {
+        const created_at = new Date();
+
+        const category = new Category({ name: 'movie', description: 'some description', is_active: true, created_at });
+
+        expect(category.props).toStrictEqual(
+            { name: 'movie', description: 'some description', is_active: true, created_at }
+        );
+    });
 });
--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,6 +1,32 @@
-export class Category {
+export type CategoryProps = {
     name: string;
-    constructor(name: string) {
-        this.name = name;
-    }
+    is_active?: boolean;
+    description?: string;
+    created_at?: Date;
 }
+
+export class Category {
+
+    constructor(public readonly props: CategoryProps) {
+        Object.freeze(this);
+    }
+
+    get name (): string {
+        return this.props.name;
+    }
+
+    get description (): string | undefined {
+        return this.props.description;
+    }
+
+    get is_active () : boolean | undefined {
+        return this.props.is_active;
+    }
+
+    get created_at () : Date | undefined {
+        return this.props.created_at;
+    }
+    
+}
+
+//create
