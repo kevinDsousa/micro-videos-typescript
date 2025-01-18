b9927aad70f3d6f17327883ae5b3d45bfa375bd7
kevinDsousa <kevindiegodasilvasousa@gmail.com>
2025-01-18 13:54:42 -0300

    test(testes de unidade): :white_check_mark: Ajeitando os testes que falharam de object

--- src/@seedwork/domain/utils/object.ts
+++ src/@seedwork/domain/utils/object.ts
@@ -1,12 +1,17 @@
 export function deepFreeze<T>(object: T): T {
+  if (object === null || object === undefined) {
+    return object;
+  }
+
   const propsNames = Object.getOwnPropertyNames(object);
 
   propsNames.forEach(name => {
     const prop = object[name as keyof T];
 
-    if (prop && typeof prop === 'object') {
+    if (typeof prop === 'object' && prop !== null) {
       deepFreeze(prop);
     }
   });
+
   return Object.freeze(object);
 }
